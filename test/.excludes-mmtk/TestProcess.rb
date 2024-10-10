7311939b42364ee18be8876aecb8c18f73a4322a
Peter Zhu <peter@peterzhu.ca>
2024-10-10 16:13:39 -0400

    Create test/.excludes-mmtk

---
+++ test/.excludes-mmtk/TestProcess.rb
@@ -0,0 +1,3 @@
+exclude(:test_warmup_frees_pages, "testing behaviour specific to default GC")
+exclude(:test_warmup_promote_all_objects_to_oldgen, "testing behaviour specific to default GC")
+exclude(:test_warmup_run_major_gc_and_compact, "testing behaviour specific to default GC")
